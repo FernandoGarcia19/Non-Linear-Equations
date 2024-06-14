80a62e211f1fb368d7beb2458709e336d6721868
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-06-13 22:50:00 -0400

    Implement Menu

--- script.py
+++ script.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os
+import msvcrt
 h = 0.005
+ACCURACY = 1000
 
 def ObjectiveFunction(x):
     result = np.exp(-x) - np.log(x) 
     return result
 
 def Biseccion(a, b, lim):
-    if(ObjectiveFunction(a)*ObjectiveFunction(b) > 0):
+    if(ObjectiveFunction(a)*ObjectiveFunction(b) < 0):
         i = 0
         m = (a+b)/2
         while(i < lim):
@@ -23,7 +25,7 @@ def Biseccion(a, b, lim):
         return "No root"
 
 def FalsaPosicion(a, b, lim):
-    if(ObjectiveFunction(a)*ObjectiveFunction(b) > 0):
+    if(ObjectiveFunction(a)*ObjectiveFunction(b) < 0):
         i = 0
         m = a - (ObjectiveFunction(a)*(a-b))/(ObjectiveFunction(a)-ObjectiveFunction(b))
         while(i < lim):
@@ -46,6 +48,32 @@ def Newton(x0, lim):
     return res
 
 
-
-
-print(Newton(2,10))
+def MainMenu():
+    ans = ""
+    while(ans != "0"):
+        print("NON-LINEAR-EQUATION-SOLVER: MAIN MENU")
+        print("1. Biseccion")
+        print("2. Falsa Posicion")
+        print("3. Newton")
+        print("0. Salir")
+        ans = input("Opcion: ")
+        match ans:
+            case "1":
+                a = int(input("a:"))
+                b = int(input("b:"))
+                print(Biseccion(a,b,ACCURACY))
+            case "2":
+                a = int(input("a:"))
+                b = int(input("b:"))
+                print(FalsaPosicion(a,b,ACCURACY))
+            case "3":
+                x0 = int(input("x0:"))
+                print(Newton(x0, ACCURACY))
+            case "0":
+                print("Hasta pronto!")
+            case _:
+                print("Opcion no valida")
+        input()
+        os.system("cls")
+    
+MainMenu()
